f938e2e7f40ff8f2d1615d22bd330097c6979731
raj <rajatrai@umass.edu>
2021-11-04 15:22:01 -0400

    removes debugging prints, adds confidence interval output

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ import numpy as np
 from sklearn.neighbors import KernelDensity
 import json
 
+def confidence_interval(data, alpha=0.25):
+    srt = sorted(data)
+    lower = srt[int((0.5-alpha)*len(srt))]
+    upper = srt[int((0.5+alpha)*len(srt))]
+    return lower, upper
+
 if __name__ == '__main__':
     file = open('data.json')
     data = json.load(file)
 
     prices = [int(x['item_price'][:-1]) for x in data['rows']]
-    print(prices)
 
     # samples = np.arange(1, len(prices)+1)
     # print(samples)
 
     X = np.array(prices).reshape(-1, 1)
-    print(X)
 
     kde = KernelDensity(kernel='gaussian', bandwidth=1).fit(X)
     X_plot = np.linspace(min(prices),max(prices))
     log_dens = kde.score_samples(X_plot.reshape((-1,1)))
-    print(log_dens)
     plt.plot(X_plot, [10**x for x in log_dens])
     plt.show()
+
+    ci = confidence_interval(prices)
+    print(f'Confidence interval: [{ci[0]}, {ci[1]}]')
