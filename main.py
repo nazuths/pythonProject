aa7cdd85fb8b5461877e903539741e51b6726bd9
raj <rajatrai@umass.edu>
2021-11-04 14:59:37 -0400

    i come, and i fix

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ import numpy as np
 from sklearn.neighbors import KernelDensity
 import json
 
+if __name__ == '__main__':
+    file = open('data.json')
+    data = json.load(file)
 
-file = open('data.json',)
-data = json.load(file)
+    prices = [int(x['item_price'][:-1]) for x in data['rows']]
+    print(prices)
 
-prices = [int(x['item_price'][:-1]) for x in data['rows']]
+    # samples = np.arange(1, len(prices)+1)
+    # print(samples)
 
-# samples = np.arange(1, len(prices)+1)
-# print(samples)
+    X = np.array(prices).reshape(-1, 1)
+    print(X)
 
-X = np.array(prices).reshape(-1, 1)
-print(X)
-
-kde = KernelDensity(kernel='gaussian', bandwidth=0.75).fit(prices)
-X_plot = np.linspace(1,len(prices)+1)
-log_dens = kde.score_samples(X_plot.reshape((-1,1)))
-print(log_dens)
-plt.plot(X_plot, log_dens)
-plt.show()
+    kde = KernelDensity(kernel='gaussian', bandwidth=1).fit(X)
+    X_plot = np.linspace(1,len(prices)+1)
+    log_dens = kde.score_samples(X_plot.reshape((-1,1)))
+    print(log_dens)
+    plt.plot(X_plot, [10**x for x in log_dens])
+    plt.show()
